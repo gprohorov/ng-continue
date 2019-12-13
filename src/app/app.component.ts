a8eabbf51a289bfadffca92e396d441e3003401c
george <yuramail222@gmail.com>
2019-12-13 18:03:36 +0200

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+
+<h1> My routes  </h1>
+    <nav>
+      <a routerLink="/second"> Second</a>
+    </nav>
+    <router-outlet></router-outlet>
+
+  </div>`
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NewoneComponent } from './newone/newone.component';
+import { SecondoneComponent } from './secondone/secondone.component';
+import { ThirdoneComponent } from './thirdone/thirdone.component';
+import { MainoneComponent } from './mainone/mainone.component';
+import {FailureComponent} from './failure/failure.component';
 
+const appRoutes: Routes = [
+  {path: '', component: MainoneComponent},
+  {path: 'second', component: SecondoneComponent},
+  {path: 'third', component: ThirdoneComponent},
+  {path: '**', component: FailureComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewoneComponent,
+    SecondoneComponent,
+    ThirdoneComponent,
+    MainoneComponent,
+    FailureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
